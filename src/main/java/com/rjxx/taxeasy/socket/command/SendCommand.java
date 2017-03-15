36d307e545d7732c4af466cc4a64f7c886538b8c
kangzhongxu <kangzhongxu123>
2017-03-15 11:59:48 +0800

    新增作废，重打

--- src/main/java/com/rjxx/taxeasy/socket/command/SendCommand.java
+++ src/main/java/com/rjxx/taxeasy/socket/command/SendCommand.java
@@ -5,6 +5,6 @@ package com.rjxx.taxeasy.socket.command;
  */
 public enum SendCommand {
     //开具发票，重新打印，设置加密的key，踢出登录，获取发票代码发票号码
-    Invoice, SetDesKey, Logout, GetCodeAndNo, VoidInvoice
+    Invoice, SetDesKey, Logout, GetCodeAndNo, VoidInvoice,RepeatInvoice
 
 }
