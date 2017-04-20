d46222a1d0c0ca2aaab971ef60ddad595d884c6a
张兵 <zhangbing@datarj.com>
2017-04-20 11:01:34 +0800

    暂时提交

--- src/main/java/com/rjxx/taxeasy/controller/UploadFileController.java
+++ src/main/java/com/rjxx/taxeasy/controller/UploadFileController.java
@@ -1,10 +1,13 @@
 package com.rjxx.taxeasy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 /**
  * Created by Administrator on 2017-04-19.
@@ -13,18 +16,22 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/uploadFile")
 public class UploadFileController {
 
+    @Value("${client-log.save.path:/mnt/client_logs}")
+    private String clientLogSavePath;
+
     /**
      * 上传文件请求
      *
-     * @param file
-     * @param kpdid
-     * @param fileName
+     * @param file  问阿金
+     * @param kpdid 开票点id
+     * @param wjlx  文件类型
      * @return
      */
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
-    public String upload(MultipartFile file, int kpdid, String fileName) {
-
+    public String upload(MultipartFile file, int kpdid, String wjlx, String date) {
+//        String savePath =
+//        file.transferTo();
 
         return "1";
     }
