ad16826183ef5d94d421f5b28a971cc92ca351b2
许黎明 <1084854604@qq.com>
2017-12-04 15:39:01 +0800

    sap回写url，福维克发短信。

--- src/main/java/com/rjxx/taxeasy/socket/command/receive/ReturnInvoiceFileCommand.java
+++ src/main/java/com/rjxx/taxeasy/socket/command/receive/ReturnInvoiceFileCommand.java
@@ -238,7 +238,7 @@ public class ReturnInvoiceFileCommand implements ICommand {
                              String ss= HttpUtils.netWebService(url,"CallBack",returnmessage,gsxx.getAppKey(),gsxx.getSecretKey());
                              String fwkReturnMessageStr=fwkReturnMessage(kpls);
                             logger.info("----------sap回写报文----------" + fwkReturnMessageStr);
-                            String Data= HttpUtils.doPostSoap1_2("https://my337109.sapbydesign.com/sap/bc/srt/scs/sap/yyb40eysay_managegoldentaxinvo?sap-vhost=my337109.sapbydesign.com", fwkReturnMessageStr, null,"wendy","Welcome9");
+                            String Data= HttpUtils.doPostSoap1_2(gsxx.getSapcallbackurl(), fwkReturnMessageStr, null,"wendy","Welcome9");
                             logger.info("----------fwk平台回写返回报文--------" + ss);
                             logger.info("----------sap回写返回报文----------" + Data);
 
